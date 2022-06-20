b2905ed818a586232a1db947823ecb49f4d66167
Ramsai-M <ramsaimuthyala97@gmail.com>
2022-06-20 14:17:51 +0530

    Adding new HTML Project called Temp to git.

---
+++ temp.js
@@ -0,0 +1,13 @@
+function myFunction(){
+	document.getElementById("demo").innerHTML="This is Permanent Page.";
+}
+function myFunction1(){
+	document.write("<p style='text-align:center;'>Content was deleted successfully</p>.");
+}
+function alert1(){
+	window.alert("This is Pop message from the browser.");
+}
+function printer(){
+	window.print();
+}
+	
